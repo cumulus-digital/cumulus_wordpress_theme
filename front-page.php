913891c21fe83c9b23a32667dab38dcade636e71
Daniel Vena <deaconvena+github@gmail.com>
2019-05-17 16:48:23 -0400

    Support user-editable hero video on front page

--- front-page.php
+++ front-page.php
@@ -2,15 +2,30 @@
 namespace CumulusTheme;
 
 \get_header();
+$custom_fields = \get_fields();
+$header_videos = array();
+foreach($custom_fields as $key => $field) {
+	if (
+		(strstr($key, 'header_video_id') || strstr($key, 'header_video_alt')) &&
+		! empty($field)
+	) {
+		$header_videos[$key] = $field;
+	}
+}
 ?>
 <section class="row hero">
 
-	<div class="video-container">
-		<video autoplay="" loop="" muted="" id="myVideo" class="landing" data-keepplaying="" data-autoplay="">
-		  <source src="http://mrdrnewyork.com/sandbox/cumulus/wp-content/uploads/2019/04/Comp-1_2.mp4" type="video/mp4">
-		  <img src="<?php echo \get_template_directory_uri() ?>/assets/prod/images/videohero.jpg">
-		</video>
-	</div>
+	<?php if (count($header_videos)): ?>
+		<div class="video-container">
+			<video autoplay="<?php $custom_fields['header_video_autoplay'] ?>" loop="" muted="" id="header_video" class="landing" data-keepplaying="" data-autoplay="">
+				<?php foreach($header_videos as $video_id): ?>
+					<?php $video = \get_post($video_id) ?>
+					<source src="<?php echo $video->guid ?>" type="<?php echo $video->post_mime_type ?>">
+				<?php endforeach ?>
+				<img src="<?php echo \get_template_directory_uri() ?>/assets/prod/images/bg-video_hero.png">
+			</video>
+		</div>
+	<?php endif ?>
 
 </section>
 
