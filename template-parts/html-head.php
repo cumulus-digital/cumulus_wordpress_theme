5aff5a6851a1fb0f8e3c01e7e7e7d8965be0709c
Daniel Vena <deaconvena+github@gmail.com>
2019-05-17 16:48:40 -0400

    favicon

--- template-parts/html-head.php
+++ template-parts/html-head.php
@@ -7,20 +7,15 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 	<meta name="description" content="<?php bloginfo('description'); ?>">
 
-	<link rel="apple-touch-icon" href="<?=\get_template_directory_uri()?>/assets/prod/images/favicons/apple-touch-icon.png?v=jw7E95zRQe">
-	<link rel="apple-touch-icon" sizes="180x180" href="<?=\get_template_directory_uri()?>/assets/prod/images/favicons/apple-touch-icon-180x180.png?v=jw7E95zRQe">
-	<link rel="icon" type="image/png" sizes="32x32" href="<?=\get_template_directory_uri()?>/assets/prod/images/favicons/favicon-32x32.png?v=jw7E95zRQe">
-	<link rel="icon" type="image/png" sizes="194x194" href="<?=\get_template_directory_uri()?>/assets/prod/images/favicons/favicon-194x194.png?v=jw7E95zRQe">
-	<link rel="icon" type="image/png" sizes="192x192" href="<?=\get_template_directory_uri()?>/assets/prod/images/favicons/android-chrome-192x192.png?v=jw7E95zRQe">
-	<link rel="icon" type="image/png" sizes="16x16" href="<?=\get_template_directory_uri()?>/assets/prod/images/favicons/favicon-16x16.png?v=jw7E95zRQe">
-	<link rel="manifest" href="<?=\get_template_directory_uri()?>/assets/prod/images/favicons/site.webmanifest?v=jw7E95zRQe">
-	<link rel="mask-icon" href="<?=\get_template_directory_uri()?>/assets/prod/images/favicons/safari-pinned-tab.svg?v=jw7E95zRQe" color="#00598e">
-	<link rel="shortcut icon" href="<?=\get_template_directory_uri()?>/assets/prod/images/favicons/favicon.ico?v=jw7E95zRQe">
-	<meta name="apple-mobile-web-app-title" content="Cumulus Media">
-	<meta name="application-name" content="Cumulus Media">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?=\get_template_directory_uri()?>/assets/static/images/favicons/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?=\get_template_directory_uri()?>/assets/static/images/favicons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?=\get_template_directory_uri()?>/assets/static/images/favicons/favicon-16x16.png">
+	<link rel="manifest" href="<?=\get_template_directory_uri()?>/assets/static/images/favicons/site.webmanifest">
+	<link rel="mask-icon" href="<?=\get_template_directory_uri()?>/assets/static/images/favicons/safari-pinned-tab.svg" color="#00598e">
+	<link rel="shortcut icon" href="<?=\get_template_directory_uri()?>/assets/static/images/favicons/favicon.ico">
 	<meta name="msapplication-TileColor" content="#00598e">
-	<meta name="msapplication-config" content="<?=\get_template_directory_uri()?>/assets/prod/images/favicons/browserconfig.xml?v=jw7E95zRQe">
-	<meta name="theme-color" content="#00598e">
+	<meta name="msapplication-config" content="<?=\get_template_directory_uri()?>/assets/static/images/favicons/browserconfig.xml">
+	<meta name="theme-color" content="#ffffff">
 
 	<?php \wp_head(); ?>
 </head>
