d3db318ea158a4bf5779a975a6078f537b908eda
Daniel Vena <deaconvena+github@gmail.com>
2019-05-17 17:23:47 -0400

    keep theme updated

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 namespace CumulusTheme;
 
 \add_filter( 'auto_update_plugin', '__return_true' );
+\add_filter( 'auto_update_theme', '__return_true' );
 
 // Helper to namespace stuff
 function ns($str) {
