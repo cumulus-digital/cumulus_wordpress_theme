6399847fa758ae55bc387ac23adba9b827bff420
Daniel Vena <deaconvena+github@gmail.com>
2019-05-19 15:25:05 -0400

    added full width w/ padding template

---
+++ template-full_width-padded.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Template Name: Full Width (w/padding)
+ * Description: Not quite a full-width page.
+ */
+namespace CumulusTheme;
+BodyClasses::add('full-width-padded');
+?>
+<?php \get_template_part('page') ?>
