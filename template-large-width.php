c406f6ecb3cc299c140b54e972987777b77d78aa
Daniel Vena <deaconvena+github@gmail.com>
2019-05-19 15:27:58 -0400

    added large-width page

---
+++ template-large-width.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Template Name: Large Width
+ * Description: Larger than standard width
+ */
+namespace CumulusTheme;
+BodyClasses::add('large-width');
+?>
+<?php \get_template_part('page') ?>
