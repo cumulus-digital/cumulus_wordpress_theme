7cfbd26a8bcdd356cc61f431fd39aec5106ee84e
Daniel Vena <deaconvena+git@gmail.com>
2019-10-30 16:02:52 -0400

    built in site icon support

--- template-parts/html-head.php
+++ template-parts/html-head.php
@@ -7,15 +7,19 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 	<meta name="description" content="<?php bloginfo('description'); ?>">
 
-	<link rel="apple-touch-icon" sizes="180x180" href="<?=\get_template_directory_uri()?>/assets/static/favicon/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="<?=\get_template_directory_uri()?>/assets/static/favicon/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="<?=\get_template_directory_uri()?>/assets/static/favicon/favicon-16x16.png">
-	<link rel="manifest" href="<?=\get_template_directory_uri()?>/assets/static/favicon/site.webmanifest">
-	<link rel="mask-icon" href="<?=\get_template_directory_uri()?>/assets/static/favicon/safari-pinned-tab.svg" color="#00598e">
-	<link rel="shortcut icon" href="<?=\get_template_directory_uri()?>/assets/static/favicon/favicon.ico">
-	<meta name="msapplication-TileColor" content="#00598e">
-	<meta name="msapplication-config" content="<?=\get_template_directory_uri()?>/assets/static/favicon/browserconfig.xml">
-	<meta name="theme-color" content="#ffffff">
+	<?php if (\has_site_icon()): ?>
+		<?php \wp_site_icon() ?>
+	<?php else: ?>
+		<link rel="apple-touch-icon" sizes="180x180" href="<?=\get_template_directory_uri()?>/assets/static/favicon/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="<?=\get_template_directory_uri()?>/assets/static/favicon/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="<?=\get_template_directory_uri()?>/assets/static/favicon/favicon-16x16.png">
+		<link rel="manifest" href="<?=\get_template_directory_uri()?>/assets/static/favicon/site.webmanifest">
+		<link rel="mask-icon" href="<?=\get_template_directory_uri()?>/assets/static/favicon/safari-pinned-tab.svg" color="#00598e">
+		<link rel="shortcut icon" href="<?=\get_template_directory_uri()?>/assets/static/favicon/favicon.ico">
+		<meta name="msapplication-TileColor" content="#00598e">
+		<meta name="msapplication-config" content="<?=\get_template_directory_uri()?>/assets/static/favicon/browserconfig.xml">
+		<meta name="theme-color" content="#ffffff">
+	<?php endif ?>
 
 	<?php \wp_head(); ?>
 </head>
