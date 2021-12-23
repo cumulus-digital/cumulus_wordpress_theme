78e0767b5d54515928cc4186e1034eada9fca202
Daniel Vena <deaconvena+github@gmail.com>
2021-12-23 15:05:58 -0500

    increase font size of card bodies

--- template-parts/card.php
+++ template-parts/card.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace CumulusTheme;
+
 $card_options = \get_query_var('card_options');
 $custom_fields = \get_fields();
 $card_image_id = $custom_fields['card_image'];
@@ -8,54 +10,54 @@ $card_image_id = $custom_fields['card_image'];
 <?php if ($card_options['links']): ?>
 
 	<a href="<?php \the_permalink() ?>" class="card <?php
-		echo 'card-display-' . $card_options['display_type'] . ' ';
-		echo $card_options['show_title'] ? 'card-show_title ' : 'card-no_title ';
-		echo $card_options['show_excerpt'] ? 'card-show_excerpt ' : 'card-no_excerpt ';
-		echo $card_image_id ? 'card-has_image ' : 'card-no_image ';
-	?>">
+        echo 'card-display-' . $card_options['display_type'] . ' ';
+        echo $card_options['show_title'] ? 'card-show_title ' : 'card-no_title ';
+        echo $card_options['show_excerpt'] ? 'card-show_excerpt ' : 'card-no_excerpt ';
+        echo $card_image_id ? 'card-has_image ' : 'card-no_image ';
+    ?>">
 
 <?php else: ?>
 
 	<div class="card <?php
-		echo 'card-display-' . $card_options['display_type'] . ' ';
-		echo $card_options['show_title'] ? 'card-show_title ' : 'card-no_title ';
-		echo $card_options['show_excerpt'] ? 'card-show_excerpt ' : 'card-no_excerpt ';
-		echo $card_image_id ? 'card-has_image ' : 'card-no_image ';
-	?>">
+        echo 'card-display-' . $card_options['display_type'] . ' ';
+        echo $card_options['show_title'] ? 'card-show_title ' : 'card-no_title ';
+        echo $card_options['show_excerpt'] ? 'card-show_excerpt ' : 'card-no_excerpt ';
+        echo $card_image_id ? 'card-has_image ' : 'card-no_image ';
+    ?>">
 
 <?php endif ?>
 
 		<?php
-			if ($card_options['show_image'] && $card_image_id):
-		?>
+            if ($card_options['show_image'] && $card_image_id):
+        ?>
 			<?php $card_image = \get_post($card_image_id) ?>
 			<figure
 				class="card-image"
 			>
-				<img 
+				<img
 					src="<?php echo function_exists('jetpack_photon_url') ? \jetpack_photon_url($card_image->guid) : $card_image->guid ?>"
 					alt="<?php
-						if (empty(get_arr_val($custom_fields, 'alt_title'))) {
-							\the_title();
-						} else {
-							echo \esc_html(get_arr_val($custom_fields, 'alt_title'));
-						}
-					?>"
+                        if (empty(get_arr_val($custom_fields, 'alt_title'))) {
+                            \the_title();
+                        } else {
+                            echo \esc_html(get_arr_val($custom_fields, 'alt_title'));
+                        }
+                    ?>"
 				>
 			</figure>
 		<?php endif ?>
 
 		<?php if ($card_options['display_type'] !== 'image_only' || ! $card_image_id): ?>
-			<?php if ( ! $card_image_id || $card_options['show_title'] || ( \has_excerpt() && $card_options['show_excerpt'] )): ?>
+			<?php if (! $card_image_id || $card_options['show_title'] || (\has_excerpt() && $card_options['show_excerpt'])): ?>
 				<div class="card-content">
 					<?php if ($card_options['show_title'] || ! $card_image_id): ?>
-						<h3>
+						<h2>
 							<?php if (empty(get_arr_val($custom_fields, 'alt_title'))): ?>
 								<?php \the_title() ?>
 							<?php else: ?>
 								<?php echo \esc_html(get_arr_val($custom_fields, 'alt_title')) ?>
 							<?php endif ?>
-						</h3>
+						</h2>
 					<?php endif ?>
 					<?php if (\has_excerpt() && $card_options['show_excerpt']): ?>
 						<div class="body">
@@ -82,7 +84,7 @@ $card_image_id = $custom_fields['card_image'];
 		<?php endif ?>
 
 <?php if ($card_options['links']): ?>
-	
+
 	</a>
 
 <?php else: ?>
