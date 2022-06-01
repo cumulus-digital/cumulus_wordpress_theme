53816c0059568644aacb86b1899a667a5302ec6c
Daniel Vena <deaconvena@gmail.com>
2022-06-01 16:21:44 -0400

    updated google font url

--- functions.php
+++ functions.php
@@ -390,7 +390,7 @@ function scripts_and_styles()
 
         \wp_register_style(
             'google_montserrat_font',
-            'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,400;0,700;1,100;1,200;1,400;1,700&display=swap',
+            'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,500;0,600;0,700;0,800;0,900;1,100;1,300;1,500;1,600;1,700;1,800;1,900&display=swap',
             false,
             null,
             'all'
@@ -405,7 +405,7 @@ function setupGutenberg()
 {
     \wp_register_Style(
         'google_montserrat_font',
-        'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,400;0,700;1,100;1,200;1,400;1,700&display=swap',
+        'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,500;0,600;0,700;0,800;0,900;1,100;1,300;1,500;1,600;1,700;1,800;1,900&display=swap',
         false,
         null,
         'all'
