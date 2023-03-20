9b40fc8b72a15a49ab774074cf6c72f6cb22dcd6
Daniel Vena <deaconvena+github@gmail.com>
2023-03-20 14:55:43 -0400

    last-modified reflects content modification date

--- functions.php
+++ functions.php
@@ -634,3 +634,43 @@ class BodyClasses
         return self::$classes;
     }
 }
+
+/**
+ * Handle some HTTP header stuff
+ */
+add_action('wp_headers', function () {
+    // Don't send these headers in the admin
+    if (\is_user_logged_in() || \is_admin()) {
+        return;
+    }
+
+    /*
+     * Remove X-Powered-By which may expose PHP version
+     */
+    \header_remove('X-Powered-By');
+
+    /*
+     * X-DNS-Prefetch-Control - Proactively perform domain name resolution on both
+     * links that the user may choose to follow as well as URLs for items
+     * referenced by the document
+     */
+    \header('X-DNS-Prefetch-Control: on');
+
+    /**
+     * Modicrom of support for old IE
+     */
+    \header('X-UA-Compatible: IE=edge,chrome=1');
+});
+
+/**
+ * Last-modified should reflect content
+ */
+\add_action('send_headers', function () {
+    if (! \is_admin() && \is_singular()) {
+        $last_modified = \get_post_modified_time('D, d M Y H:i:s', true);
+
+        if ($last_modified) {
+            \header('Last-Modified: ' . $last_modified . ' GMT');
+        }
+    }
+});
