10d7f72140dcb4857038a6a616da44b10af575d7
Daniel Vena <deaconvena+github@gmail.com>
2023-07-11 16:37:47 -0400

    add wp_body_open

--- header.php
+++ header.php
@@ -1,12 +1,23 @@
 <?php
+
 namespace CumulusTheme;
 
 \get_template_part('template-parts/html-head');
 
 ?>
 <body <?php \body_class(BodyClasses::get()); ?>>
-	<?php if ( function_exists( 'gtm4wp_the_gtm_tag' ) ) { \gtm4wp_the_gtm_tag(); } ?>
+<?php
+    if (\function_exists('wp_body_open')) {
+        \wp_body_open();
+    } else {
+        \do_action('wp_body_open');
+    }
+?>
+
+<?php
+    if (\function_exists('gtm4wp_the_gtm_tag')) {
+        \gtm4wp_the_gtm_tag();
+    }
+?>
 
 	<?php \get_template_part('template-parts/masthead') ?>
-
-
