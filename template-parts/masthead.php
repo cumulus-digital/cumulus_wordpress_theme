a3c7a645da17f232828e39710eb553b25e337624
Daniel Vena <deaconvena+github@gmail.com>
2023-08-10 23:11:37 -0400

    fix header class

--- template-parts/masthead.php
+++ template-parts/masthead.php
@@ -3,7 +3,7 @@
 namespace CumulusTheme;
 
 ?>
-<header class="masthead <?php BodyClasses::has( 'post_header_image' ) || \is_front_page() ? 'trans' : 'solid'; ?>">
+<header class="masthead <?php echo BodyClasses::has( 'post_header_image' ) || \is_front_page() ? 'trans' : 'solid'; ?>">
 
 	<?php \get_template_part( 'template-parts/masthead', 'inner' ); ?>
 
