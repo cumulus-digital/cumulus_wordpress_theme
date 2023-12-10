5813227a0a934fc3206fef2c8c9db3d543162a2f
Daniel Vena <deaconvena+github@gmail.com>
2023-12-09 23:00:37 -0500

    fix check for real preload tags, run swap earlier

--- functions/setup/filters/display/style-loader-tag.php
+++ functions/setup/filters/display/style-loader-tag.php
@@ -7,13 +7,12 @@ namespace CumulusTheme;
 
 \defined( 'ABSPATH' ) || exit( 'No direct access allowed.' );
 
-\add_filter( 'style_loader_tag', function ( $tag, $handle, $href, $media ) {
-	if ( \get_option( 'cmls-async_fonts', '1' ) === '1' ) {
+if ( \get_option( 'cmls-async_fonts', '1' ) === '1' ) {
+	\add_filter( 'style_loader_tag', function ( $tag, $handle, $href, $media ) {
 		// Don't alter real preload tags or ones with existing onload attributes
 		if (
-			\mb_stristr( $tag, 'media="preload"' )
-			|| \mb_stristr( $tag, "media='preload'" )
-			|| \mb_stristr( 'onload', $tag )
+			\mb_stristr( $tag, 'rel="preload"' )
+			|| \mb_stristr( $tag, "rel='preload'" )
 		) {
 			return $tag;
 		}
@@ -37,28 +36,30 @@ namespace CumulusTheme;
 			$noscript = \str_ireplace( $replace_media, 'media="all"', $tag );
 			$onload   = \str_ireplace( $replace_media, 'media="print" data-preloading', $tag );
 
-			\wp_register_script( PREFIX . '-preload-style', '', array( ), false, true );
-			\wp_enqueue_script( PREFIX . '-preload-style', '', array( ), false, true );
-
-			\wp_add_inline_script(
-				PREFIX . '-preload-style',
-				"
-document.addEventListener('DOMContentLoaded', function () {
-	var styles = document.querySelectorAll('link[data-preloading]');
-	[].forEach.call(styles, function(s) {
-		if (s.sheet) {
-			s.media='all';
-		} else {
-			s.addEventListener('load', function() { this.media = 'all'; });
-		}
-	});
-});
-"
-			);
-
 			return "{$onload}\n<noscript>{$noscript}</noscript>";
 		}
-	}
 
-	return $tag;
-}, \PHP_INT_MAX, 4 );
+		return $tag;
+	}, \PHP_INT_MAX, 4 );
+
+	\add_action( 'wp_enqueue_scripts', function () {
+		\wp_register_script( PREFIX . '-preload-style', '', array( ), false, false );
+		\wp_enqueue_script( PREFIX . '-preload-style', '', array( ), false, false );
+
+		\wp_add_inline_script(
+			PREFIX . '-preload-style',
+			"
+	document.addEventListener('DOMContentLoaded', function () {
+		var styles = document.querySelectorAll('link[data-preloading]');
+		[].forEach.call(styles, function(s) {
+			if (s.sheet) {
+				s.media='all';
+			} else {
+				s.addEventListener('load', function() { this.media = 'all'; });
+			}
+		});
+	});
+		"
+		);
+	}, 1 );
+}
